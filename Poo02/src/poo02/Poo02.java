b59a3e5d739a06d3fb42b99a742de7e6938f0016
Andre <alves.andre.felipe@hotmail.com>
2019-09-30 20:26:32 -0300

    Implementando classes

---
+++ Poo02/src/poo02/Poo02.java
@@ -0,0 +1,30 @@
+
+package poo02;
+
+public class Poo02 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor="Azul";
+        c1.ponta=0.5f;
+        c1.tampar();
+        //Atributo é atribuido com simbolo de =
+        //Método recebe um parametro podendo colocar em "()" 
+        //mesmo que nao tenha parametro tem que colocar "()"
+        c1.rabiscar();
+        
+        //c1.status();
+        c1.status(); 
+        
+       Caneta c2 = new Caneta();
+       c2.modelo = "Bic";
+       c2.cor = "preta";
+       c2.destampar();
+       c2.rabiscar();
+       
+       c2.status();
+        
+    }
+    
+    
+}
